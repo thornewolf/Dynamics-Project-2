80409b24c2970c559df23c9c8efe5643ef103b83
Thorne Wolf <thornecanyon@gmail.com>
2018-11-15 14:03:12 -0700

    Added Jeff and Trey linearization work

--- Project 2/Code/rigid_linearized.m
+++ Project 2/Code/rigid_linearized.m
@@ -41,18 +41,18 @@ Fs_nP = simplify(FsR(1)*ehatnP(1)+FsR(2)*ehatnP(2));
 Fs_tP = simplify(FsR(1)*ehattP(1)+FsR(2)*ehattP(2));
 
 % Sum of Normal Forces, left bar (Theta)
-eqn(1) = m*(l/2)*thetadot^2 == Fs_nT + An - m*g*cos(theta);
+eqn(1) = m*(l/2)*thetadot^2 == Fs_nT + An - m*g;
 % Sum of Tangential Forces, left bar (Theta)
-eqn(2) = m*(l/2)*thetaddot == Fs_tT + At - m*g*sin(theta);
+eqn(2) = m*(l/2)*thetaddot == Fs_tT + At - m*g*(theta);
 % Sum of Moments, left bar (Theta)
-eqn(3) = -(l/2)*(m*g*sin(theta)) + (Fs_tT)*(l) == (1/3)*m*(l^2)*thetaddot;
+eqn(3) = -(l/2)*(m*g*(theta)) + (Fs_tT)*(l) == (1/3)*m*(l^2)*thetaddot;
 
 % Sum of Normal Forces, right bar (Phi)
-eqn(4) = m*(l/2)*phidot^2 == Fs_nP + Cn - m*g*cos(phi);
+eqn(4) = m*(l/2)*phidot^2 == Fs_nP + Cn - m*g;
 % Sum of Tangential Forces, right bar (Phi)
-eqn(5) = m*(l/2)*phiddot == Fs_tP + Ct - m*g*sin(phi);
+eqn(5) = m*(l/2)*phiddot == Fs_tP + Ct - m*g*(phi);
 % Sum of Moments, right bar (Phi)
-eqn(6) = -(l/2)*(m*g*sin(phi)) + (Fs_tP)*(l) == (1/3)*m*(l^2)*phiddot;
+eqn(6) = -(l/2)*(m*g*(phi)) + (Fs_tP)*(l) == (1/3)*m*(l^2)*phiddot;
 
 x = solve(eqn,[thetaddot,phiddot,An,At,Cn,Ct]);
 
@@ -66,6 +66,33 @@ eom = odeFunction([thetadot; thetaEOM; phidot; phiEOM],...
 
 theta_o = [pi/12 -pi/12 pi/36];
 
+[T,S] = ode45(@(t,s)eom(t,s,c.m,c.k,c.l,c.I,c.g),linspace(0,10,1001),...
+    [theta_o(1),0,pi/12,0]);
+
+% Mass Matrix
+m = [.25 0;0 .25];
+% Acceleration Matrix
+acc = [thetaddot;phiddot];
+% Stiffness Matrix
+k = [25 0;0 -25];
+% Displacement of Spring
+disp = [k*(ls-l);k*(ls-l)];
+
+[V,D] = eig(k,m)
+
+eigvec1 = V(:,1)
+eigvec2 = V(:,2)
+
+% w = sqrt(D)
+
+syms c1 c2 psi1 psi2 t
+
+eqn1(1) = pi/12 == c1*D(1,1)*cos(-10*t + psi1) + c2*D(1,2)*cos(0*t + psi2);
+eqn1(2) = pi/12 == c1*D(2,1)*cos(0*t + psi1) + c2*D(2,2)*cos(10*t + psi2);
+
+x1 = solve(eqn1,[c1,c2,psi1,psi2]);
+
+
 
 % for i = 1:3
 %     [T,S] = ode45(@(t,s)eom(t,s,c.m,c.k,c.l,c.I,c.g),linspace(0,10,1001),...
@@ -73,16 +100,17 @@ theta_o = [pi/12 -pi/12 pi/36];
 %     figure
 %     hold on
 %     grid on
+% %     title('\theta and \phi vs Time')
 %     xlabel('Time, sec')
 %     ylabel('Angular Position, rad')
 %     plot(T,S(:,1),'DisplayName', ['\theta_o = ' num2str(rad2deg(theta_o(i))) '^o'])
 %     plot(T,S(:,3),'DisplayName', '\phi_o = 15^o')
 %     legend('show')
-%     set(gcf, 'PaperPositionMode', 'manual');
-%     set(gcf, 'PaperUnits', 'inches');
-%     set(gcf, 'PaperPosition', [1 1 6 2.5]);
-%     fig = gcf;
-%     print(['BestFitFigure' num2str(i)],'-dpdf');
+% %     set(gcf, 'PaperPositionMode', 'manual');
+% %     set(gcf, 'PaperUnits', 'inches');
+% %     set(gcf, 'PaperPosition', [1 1 6 2.5]);
+% %     fig = gcf;
+% %     print(['BestFitFigure' num2str(i)],'-dpdf');
 % end
 % 
 % 
